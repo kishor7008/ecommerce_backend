031a740a3e17b763d062ab157fb7c0a7ea5640cd
kishor-7008 <kishor@hminnovance.com>
2023-05-30 11:18:05 +0530

    kdfkwf

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,4 @@
-const User = require("../models/UserModel");
+const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
 const generateToken = require("../utils/generateToken");
 
